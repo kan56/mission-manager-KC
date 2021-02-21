caf00a19e8844df409d08632a4095f986c7754df
DESKTOP-1RI56QV\kanta <kanta510g@gmail.com>
2021-02-22 02:11:27 +0900

    ボタンにdisabledを追加

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
-window.alert("hello,world!");
+console.log("hello,world!");
 
 $(function(){
     $(".btn").on("click",function(){
-        $(this).css("color","red");
+        if($(this).hasClass("disabled")){
+            if(confirm($(this).text() + "を未達成の状態に戻しますか？")){
+                $(this).removeClass("disabled");
+            }
+        }else{
+            $(this).addClass("disabled");
+        }
+        
     });
 });
