d83ac1c7519b7239e4a1a209d4f04473dfa97df6
DESKTOP-1RI56QV\kanta <kanta510g@gmail.com>
2021-02-22 02:30:10 +0900

    デイリー任務に残り時間表示を追加

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 console.log("hello,world!");
 
+var time = new Date();
+
 $(function(){
     $(".btn").on("click",function(){
         if($(this).hasClass("disabled")){
@@ -9,6 +11,7 @@ $(function(){
         }else{
             $(this).addClass("disabled");
         }
-        
     });
+
+    $("#dayrytime").text("残り" + (24 - time.getHours()) + "時間" + (60 - time.getMinutes()) + "分");
 });
