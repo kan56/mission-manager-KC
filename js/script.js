49b81618245c8074bab6eb9cf9b290f32a2562da
DESKTOP-1RI56QV\kanta <kanta510g@gmail.com>
2021-02-25 19:59:00 +0900

    コメントを追加

--- js/script.js
+++ js/script.js
@@ -16,7 +16,9 @@ var datas = [
 //現在の時間を格納する変数を作成
 var time = new Date();
 
+//datasの内容を反映させる
 function rerodeData() {
+    //データの数ループを回し、ボタンに状態に応じたスタイルを反映   
     for(var i = 0;i < datas.length;i ++){
         for(var j = 0;j < datas[i].m.length;j ++){
             if(datas[i].m[j] == 1 && $($(datas[i].id)[0].children.item(j)).hasClass("btn-primary")){
@@ -25,6 +27,7 @@ function rerodeData() {
                 $($(datas[i].id)[0].children.item(j)).removeClass("btn-secondary").removeClass("disabled").addClass("btn-primary");
             }
         }
+        //任務が達成されているなら、クリアを表示
         if(datas[i].m.filter(x => x == 1).length == datas[i].m.length && $(datas[i].id).next().css("display") != "block"){
             $(datas[i].id).next().show();
         }else if(datas[i].m.filter(x => x == 1).length != datas[i].m.length && $(datas[i].id).next().css("display") == "block"){
@@ -47,13 +50,13 @@ $(function(){
         $(".crear").hide();
     }
 
-
-    $(".dayryBtn").on("click",function() { 
+    //任務ボタンが押されたときの処理
+    $(".dayryBtn").on("click",function() {
+        //押されたボタンに対応するdatasのインデックス番号を取得
         var index = $(this).parent().attr("id");
         index = Number(index.slice(1,index.length));
-        console.log(datas[index-1]);
-        console.log($(this).parent().children("button").index(this));
-        datas[index-1].m[$(this).parent().children("button").index(this)] 
+
+        //未達成と達成を切り替える
         if(datas[index-1].m[$(this).parent().children("button").index(this)] == 1){
             if(confirm($(this).text() + "を未達成の状態に戻しますか？")){
                 datas[index-1].m[$(this).parent().children("button").index(this)] = 0;
@@ -61,35 +64,16 @@ $(function(){
         }else{
             datas[index-1].m[$(this).parent().children("button").index(this)] = 1;
         }
+        
+        //ローカルストレージに保存
         localStorage.setItem("datas",JSON.stringify(datas));
         rerodeData();
     });
-    // $(".dayryBtn").on("click",function(){
-    //     if($(this).hasClass("disabled")){
-    //         if(confirm($(this).text() + "を未達成の状態に戻しますか？")){
-    //             $(this).removeClass("btn-secondary");
-    //             $(this).addClass("btn-primary");
-    //             $(this).removeClass("disabled");
-    //             if($(this).parent().next().css("display") == "block"){
-    //                 $(this).parent().next().hide();
-    //             }
-    //         }
-    //     }else{
-    //         $(this).removeClass("btn-primary").addClass("btn-secondary").addClass("disabled");
-    //         var parent = $(this).parent();
 
-    //         for(var i = 0;i < parent.children("button").length;i ++){
-    //             if(!$(parent[0].children.item(i)).hasClass("disabled")){
-    //                 break;
-    //             }else if(i === parent.children("button").length - 1){
-    //                 parent.next().show();
-    //             }
-    //         }
-    //     }
-    // });
-
+    //時間を表示
     $("#dayrytime").text("残り" + (23 - time.getHours()) + "時間" + (59 - time.getMinutes()) + "分");
 
+    //一分毎に時間を更新
     setInterval(function(){
         time = new Date();
         $("#dayrytime").text("残り" + (23 - time.getHours()) + "時間" + (59 - time.getMinutes()) + "分");
