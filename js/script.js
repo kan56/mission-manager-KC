7b729eaa671a4042bb1414fe47626eb62b80b78f
DESKTOP-1RI56QV\kanta <kanta510g@gmail.com>
2021-03-01 18:49:55 +0900

    任務のオートリセットを追加

--- js/script.js
+++ js/script.js
@@ -15,6 +15,7 @@ var datas = [
 
 //現在の時間を格納する変数を作成
 var time = new Date();
+console.log(time + "1");
 
 //datasの内容を反映させる
 function rerodeData() {
@@ -36,6 +37,45 @@ function rerodeData() {
     }
 }
 
+function reloadTime() {
+    //現在時刻を入れる関数(比較用)
+    console.log(time + "2");
+    var now = new Date();
+    console.log(time  + "3");
+    switch(true){
+        //年が変わっているとき
+        case now.getFullYear() != time.getFullYear():
+            console.log("年が変わった");
+
+        //月が変わっているとき
+        case now.getMonth() != time.getMonth():
+            console.log("月が変わった");
+
+        //日が変わっている時
+        case now.getDate() != time.getDate():
+            console.log("日が変わった");
+            for(var i = 0;i < datas.length;i ++){
+                for(var j = 0;j < datas[i].m.length;j ++){
+                    datas[i].m[j] = 0;
+                }
+            }
+
+            //週が変わっているとき
+            if(now.getTime - time.getTime > 1000*60*60*24*7){
+                console.log("週が変わった");
+            }else if(now.getDay() - time.getDay() < 1){
+                console.log("週が変わった");
+            }
+            rerodeData();
+        break;
+    }
+    console.log(time);
+    time = new Date();
+    console.log(time);
+    localStorage.setItem("datas",JSON.stringify(datas));
+    localStorage.setItem('time',JSON.stringify(time));
+}
+
 
 //読み込まれたときに実行
 $(function(){
@@ -49,6 +89,14 @@ $(function(){
     }else{
         $(".crear").hide();
     }
+    var tempTime = localStorage.getItem('time');
+    if(tempTime != null){
+        time = JSON.parse(tempTime);
+        console.log(time);
+        time = new Date(time);
+        reloadTime();
+    }
+    reloadTime();
 
     //任務ボタンが押されたときの処理
     $(".dayryBtn").on("click",function() {
@@ -75,7 +123,7 @@ $(function(){
 
     //一分毎に時間を更新
     setInterval(function(){
-        time = new Date();
+        reloadTime();
         $("#dayrytime").text("残り" + (23 - time.getHours()) + "時間" + (59 - time.getMinutes()) + "分");
     },1000*60)
 });
